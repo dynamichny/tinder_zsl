f901dac96df33ca83bdde39a9936c3836609d730
dynamiczny <marcinpawlicki0@gmail.com>
2020-01-08 00:20:05 +0100

    m0 on body xd

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,12 @@
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,700&display=swap');
+body{
+  margin: 0;
+}
 #app{
   height: 100vh;
   margin: 0;
+  padding: 0;
 }
 </style>
